98913324c8ddd75aff2ac0ba7da5ec2af27e071e
Mathieu Lefebvre <mathieu@eosnation.io>
2023-09-27 16:06:48 -0400

    change commentary

--- src/maps.rs
+++ src/maps.rs
@@ -45,7 +45,7 @@ pub fn map_contract_info(store: Deltas<DeltaString>) -> Result<Infos, Error> {
     Ok(Infos { items })
 }
 
-// ETH Call to retrieve ERC20 token name
+// ETH Call to retrieve ERC20 token Name
 pub fn get_contract_name(address: String) -> Option<String> {
     let call = abi::erc20::functions::Name{};
     log::info!("get_contract_name: {:?}", address);
@@ -61,7 +61,7 @@ pub fn get_contract_symbol(address: String) -> Option<String> {
     call.call(hex)
 }
 
-// ETH Call to retrieve ERC20 token Symbol
+// ETH Call to retrieve ERC20 token Decimal
 pub fn get_contract_decimals(address: String) -> Option<BigInt> {
     let call = abi::erc20::functions::Decimals{};
     log::info!("get_contract_decimals: {:?}", address);
