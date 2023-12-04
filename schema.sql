c7e8c8d72b5954f2efd22688b9ec2ee246c3dfbe
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2023-12-04 15:28:11 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS Contracts  (
+    chain           LowCardinality(String),
+    block_number    UInt32(),
+    timestamp       DateTime64(3, 'UTC'),
     address FixedString(40),
     name Nullable(String),
     symbol Nullable(String),
