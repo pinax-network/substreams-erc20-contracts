01e8e6cf4ab8702e57b43ed8bc30e56cb11af6f8
Mathieu Lefebvre <49442766+Matlefebvre1234@users.noreply.github.com>
2024-05-09 10:55:08 -0400

    Init substreams-sink-sql support

--- schema.sql
+++ schema.sql
@@ -1,14 +1,15 @@
 CREATE TABLE IF NOT EXISTS Contracts  (
+    "id" String,
     chain           LowCardinality(String),
     block_number    UInt32(),
     timestamp       DateTime64(3, 'UTC'),
     address FixedString(40),
     name Nullable(String),
     symbol Nullable(String),
-    decimals Nullable(UInt64)
+    decimals Nullable(UInt64),
 )
-ENGINE = MergeTree()
-ORDER BY (address, timestamp, block_number, chain);
+ENGINE = MergeTree PRIMARY KEY ("id")
+ORDER BY (id,address, timestamp, block_number, chain);
 
 -- Indexes for block_number and chain --
 ALTER TABLE Contracts ADD INDEX Contracts_block_number_index block_number TYPE minmax;
